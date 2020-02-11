85340fbd0642ce2bc63af2247c6847f513fcbe58
b9012949 <b9012949@newcastle.ac.uk>
2020-02-11 17:08:13 +0000

    Started staff member class

---
+++ src/StaffMember.java
@@ -0,0 +1,15 @@
+public class StaffMember {
+
+    String surname, initials;
+    int extension;
+
+    public StaffMember(String surname, String initials, int extension){
+        this.surname = surname;
+        this.initials = initials;
+        this.extension = extension;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
